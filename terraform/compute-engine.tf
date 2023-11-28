eeea64d34ff78bd86efdb5a49a4320bd45156825
Miouzora <alexandre.franquet@epitech.eu>
2023-11-28 18:39:20 +0100

    feat(ansible): add root of ansible (without archi)

--- terraform/compute-engine.tf
+++ terraform/compute-engine.tf
@@ -17,15 +17,15 @@ resource "google_compute_instance" "default" {
 
   tags= ["jenkins"]
 
-  metadata_startup_script = <<SCRIPT
-    sudo apt update
-    sudo apt install openjdk-11-jre -y
-    curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-        /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-    echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-        https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-        /etc/apt/sources.list.d/jenkins.list > /dev/null
-    sudo apt-get update
-    sudo apt-get install jenkins -y
-    SCRIPT
+  # metadata_startup_script = <<SCRIPT
+  #   sudo apt update
+  #   sudo apt install openjdk-11-jre -y
+  #   curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  #       /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+  #   echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  #       https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  #       /etc/apt/sources.list.d/jenkins.list > /dev/null
+  #   sudo apt-get update
+  #   sudo apt-get install jenkins -y
+  #   SCRIPT
 }
