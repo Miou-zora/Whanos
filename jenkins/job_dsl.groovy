d0884ba326ce78530ae7cb7fc769cb2c8fbd3209
Luchooooo <louis.barreteau@epitech.eu>
2023-12-01 10:41:21 +0100

    feat(core): added link-project job

--- jenkins/job_dsl.groovy
+++ jenkins/job_dsl.groovy
@@ -52,3 +52,35 @@ freeStyleJob("Whanos base images/whanos-befunge") {
         shell("echo docker build befunge image")
     }
 }
+
+freeStyleJob("link-project") {
+    parameters {
+        stringParam('REPOSITORY_URL', null, 'Git repository url to bind')
+        stringParam('DISPLAY_NAME', null, 'Display name for the job')
+    }
+    steps {
+        dsl ({
+            text('''
+                freeStyleJob(\"Projects/$DISPLAY_NAME\") {
+                    scm {
+                        git {
+                            remote {
+                                name("origin")
+                                url("$REPOSITORY_URL")
+                            }
+                        }
+                    }
+                    triggers {
+                        scm("* * * * *")
+                    }
+                    steps {
+                        shell("/check_language $DISPLAY_NAME\")
+                    }
+                    wrappers {
+                        preBuildCleanup()
+                    }
+                }
+            ''')
+        })
+    }
+}
