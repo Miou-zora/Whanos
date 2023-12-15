ad07a06b512c588984e04de4ffa4562615fb5774
Miouzora <alexandre.franquet@epitech.eu>
2023-12-15 19:05:23 +0100

    feat(kubernetes): setup ansible and jenkins to make terraform works

--- kubernetes/terraformFiles/deploy.sh
+++ kubernetes/terraformFiles/deploy.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-# TODO
+PROJECT_NAME=$1
+IMAGE_NAME=$2
+WHANOS_FILE_PATH=$3
+
+cp $WHANOS_FILE_PATH /var/lib/jenkins/kubernetes/terraform/values.yaml
+
+sed -i "s/^nameOverride:.*/nameOverride: $PROJECT_NAME/" /var/lib/jenkins/kubernetes/terraform/setup.yaml
+sed -i "s/^resource \"helm_release\".*/resource \"helm_release\" \"$PROJECT_NAME\" {/" /var/lib/jenkins/kubernetes/terraform/helm.tf
+sed -i "s/^  name       = .*/  name       = \"$PROJECT_NAME\"/" /var/lib/jenkins/kubernetes/terraform/helm.tf
+
+cd /var/lib/jenkins/kubernetes/terraform
+terraform apply -var-file="variables.tfvars" -target=helm_release.$PROJECT_NAME -auto-approve
