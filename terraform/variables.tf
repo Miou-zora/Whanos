3317854c7f08687bd3ae062e7fe0c8d6e311ce86
queng <quentin.brejoin@gmail.com>
2023-12-15 10:09:27 +0100

    refactor(terraform): update env file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,19 @@ variable "project_region" {
   type = string
   description = "The region to deploy to"
 }
+
+variable "credentials_file" {
+  type = string
+  description = "The credentials to use for deployment"
+}
+
+variable "machine_type" {
+  type = string
+  description = "The machine type to use for the instance"
+  default = "e2-medium"
+}
+
+variable "machine_zone" {
+  type = string
+  description = "The zone to deploy the instance to"
+}
